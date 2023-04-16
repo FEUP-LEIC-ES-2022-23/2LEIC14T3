c6db113c471a311770191c554964ccce2d25d803
francis802 <franciscosccampos@gmail.com>
2023-04-17 00:32:21 +0100

    Tests done, the theoretical part is not

--- app/lib/screens/credits_page.dart
+++ app/lib/screens/credits_page.dart
@@ -3,13 +3,21 @@ import 'package:flutter/material.dart';
 class CreditsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ListView(
-        children:[Text(
-          'Credits:\n\nFrancisco Campos\nDiogo Silva\nJoão Figueiredo\nTiago Simões\nPedro Plácido',
+    return ListView(
+      children: [
+        const SizedBox(height: 24),
+        const Text(
+          'Credits:',
+          textAlign: TextAlign.center,
           style: TextStyle(fontSize: 24),
-        ),]
-      ),
+        ),
+        const SizedBox(height: 24),
+        const Center(child: Text('Francisco Campos', style: TextStyle(fontSize: 24))),
+        const Center(child: Text('Diogo Silva', style: TextStyle(fontSize: 24))),
+        const Center(child: Text('João Figueiredo', style: TextStyle(fontSize: 24))),
+        const Center(child: Text('Tiago Simões', style: TextStyle(fontSize: 24))),
+        const Center(child: Text('Pedro Plácido', style: TextStyle(fontSize: 24))),
+      ],
     );
   }
 }
--- app/test_driver/steps/test_steps.dart
+++ app/test_driver/steps/test_steps.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:flutter_gherkin/flutter_gherkin.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:gherkin/gherkin.dart';
 
 
