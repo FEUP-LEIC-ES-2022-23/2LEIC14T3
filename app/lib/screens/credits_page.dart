f11f79cee98fab3abe3ad5dc47a638e0873b71ef
diogocs18 <diogopinguinhas@hotmail.com>
2023-05-20 18:43:43 +0100

    credits better

--- app/lib/screens/credits_page.dart
+++ app/lib/screens/credits_page.dart
@@ -1,24 +1,55 @@
 import 'package:flutter/material.dart';
 
 class CreditsPage extends StatelessWidget {
-  const CreditsPage({super.key});
+  const CreditsPage({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: const [
-        SizedBox(height: 24),
-        Text(
-          'Credits:',
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 24),
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          alignment: Alignment.center,
+          child: ListView(
+            shrinkWrap: true,
+            padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
+            children: const [
+              Text(
+                'Credits',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold, color: Colors.blue),
+              ),
+              SizedBox(height: 32),
+              CreditItem(icon: Icons.person, name: 'Francisco Campos'),
+              SizedBox(height: 8),
+              CreditItem(icon: Icons.person, name: 'Diogo Silva'),
+              SizedBox(height: 8),
+              CreditItem(icon: Icons.person, name: 'João Figueiredo'),
+              SizedBox(height: 8),
+              CreditItem(icon: Icons.person, name: 'Tiago Simões'),
+              SizedBox(height: 8),
+              CreditItem(icon: Icons.person, name: 'Pedro Plácido'),
+            ],
+          ),
         ),
-        SizedBox(height: 24),
-        Center(child: Text('Francisco Campos', style: TextStyle(fontSize: 24))),
-        Center(child: Text('Diogo Silva', style: TextStyle(fontSize: 24))),
-        Center(child: Text('João Figueiredo', style: TextStyle(fontSize: 24))),
-        Center(child: Text('Tiago Simões', style: TextStyle(fontSize: 24))),
-        Center(child: Text('Pedro Plácido', style: TextStyle(fontSize: 24))),
+      ),
+    );
+  }
+}
+
+class CreditItem extends StatelessWidget {
+  final IconData icon;
+  final String name;
+
+  const CreditItem({required this.icon, required this.name});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(icon, size: 32, color: Colors.blue),
+        const SizedBox(width: 8),
+        Text(name, style: const TextStyle(fontSize: 24)),
       ],
     );
   }
